11e6955c6a4743fae3710130aa1f306dee24ad44
Jiayi-Wang-Joey <61793140+Jiayi-Wang-Joey@users.noreply.github.com>
2022-04-12 10:25:13 +0200

    update test_maxent

--- tests/phirl/test_maxent.py
+++ tests/phirl/test_maxent.py
@@ -1,4 +1,3 @@
-
 from typing import List, Tuple
 import numpy as np
 import numpy.testing as nptest
@@ -8,9 +7,7 @@ import anytree
 
 import phirl.maxent as me
 import phirl.api as ph
-import trajectory
 from irl_maxent import optimizer as O
-
 
 
 @pytest.mark.parametrize("n_actions", (2, 5, 7))
@@ -22,7 +19,7 @@ def test_n_states(n_actions: int) -> None:
 
 
 @pytest.mark.parametrize("n_actions", (2, 5, 7))
-def test_state_space(n_actions) -> None:
+def test_state_space(n_actions: int) -> None:
     space = me.get_state_space(n_actions)
     size = me.get_n_states(n_actions)
 
@@ -166,6 +163,7 @@ class TestOneHotFeaturizer:
             index = featurizer.state_to_index(state)
             nptest.assert_equal(featurizer.transform(state), np.eye(len(space))[index])
 
+
 class TestTrajectory:
     def test_init_raises(self) -> None:
         with pytest.raises(ValueError):
@@ -243,8 +241,8 @@ class TestExpectedEmpiricalFeatureCounts:
 
 
 @pytest.mark.parametrize("n_actions", (2, 5, 7))
-def test_get_features(n_actions: int) -> None:
-    """ Tests for the function that mapping the state to its corresponding features """
+def test_get_features(n_actions) -> None:
+    """Tests for the function that mapping the state to its corresponding features"""
     featurizer = me.IdentityFeaturizer(n_actions)
     state_space = me.get_state_space(n_actions)
 
@@ -258,54 +256,61 @@ def test_get_features(n_actions: int) -> None:
 class TestGetPTransition:
     """Tests for function calculating the probability of transition"""
 
-    def test_2_mutation(self) -> None:
+    def test_3_mutation(self, n_actions=3) -> None:
+        mdp = me.DeterministicTreeMDP(n_actions)
+        state_space = me.get_state_space(n_actions)
+        p_transition = me.get_p_transition(n_actions, state_space=state_space, mdp=mdp)
+        current_state1 = state_space.index(tuple([0, 0, 0]))
+        next_state1 = state_space.index(tuple([1, 0, 0]))
+        action1 = 0
 
-        trajectories = [
-            [(0, 1), (1, 1)], 
-            [(0, 0), (0, 1)],
-            [(0, 0), (1, 0)],
-            [(0, 0), (1, 0), (1, 1)]
-        ]
-        
-        p_transition = me.get_p_transition(n_states = me.get_n_states(2), n_actions=2, 
-                                            state_space = me.get_state_space(2), trajectories=trajectories)
-        print(p_transition)
-        nptest.assert_equal(p_transition[0, 1, 1], 1/3)
-        nptest.assert_equal(p_transition[0, 2, 0], 2/3)
-        nptest.assert_equal(p_transition[2, 3, 1], 1)
-        nptest.assert_equal(p_transition[1, 3, 0], 1)
+        current_state2 = state_space.index(tuple([1, 0, 0]))
+        next_state2 = state_space.index(tuple([1, 0, 1]))
+        action2 = 2
 
-    def test_3_mutation(self) -> None:
-        
-        trajectories = [
-            [(0, 0, 0), (0, 1, 0)], 
-            [(0, 0, 0), (0, 0, 1)],
-            [(0, 0, 0), (1, 0, 0)],
-            [(0, 0, 0), (1, 0, 0), (1, 1, 0)]
-        ]
+        current_state3 = state_space.index(tuple([1, 0, 0]))
+        next_state3 = state_space.index(tuple([1, 0, 1]))
+        action3 = 1
 
-        p_transition = me.get_p_transition(n_states = me.get_n_states(3), n_actions=3, 
-                                            state_space = me.get_state_space(3), trajectories=trajectories)
+        nptest.assert_equal(p_transition[current_state1, next_state1, action1], 1)
+        nptest.assert_equal(p_transition[current_state2, next_state2, action2], 1)
+        nptest.assert_equal(p_transition[current_state3, next_state3, action3], 0)
 
-        nptest.assert_equal(p_transition[0, 1, 2], 1/4)
-        nptest.assert_equal(p_transition[0, 2, 1], 1/4)
-        nptest.assert_equal(p_transition[0, 4, 0], 1/2)
-        nptest.assert_equal(p_transition[4, 6, 1], 1)
+    def test_6_mutation(self, n_actions=6) -> None:
+
+        mdp = me.DeterministicTreeMDP(n_actions)
+        state_space = me.get_state_space(n_actions)
+        p_transition = me.get_p_transition(n_actions, state_space=state_space, mdp=mdp)
+        current_state1 = state_space.index(tuple([0, 0, 0, 0, 0, 0]))
+        next_state1 = state_space.index(tuple([0, 0, 0, 0, 0, 1]))
+        action1 = 5
+
+        current_state2 = state_space.index(tuple([1, 1, 1, 0, 0, 0]))
+        next_state2 = state_space.index(tuple([0, 0, 0, 0, 0, 1]))
+        action2 = 0
+
+        current_state3 = state_space.index(tuple([1, 1, 1, 0, 0, 0]))
+        next_state3 = state_space.index(tuple([1, 1, 1, 1, 0, 0]))
+        action3 = 3
+
+        nptest.assert_equal(p_transition[current_state1, next_state1, action1], 1)
+        nptest.assert_equal(p_transition[current_state2, next_state2, action2], 0)
+        nptest.assert_equal(p_transition[current_state3, next_state3, action3], 1)
 
 
 class TestPAction:
     """Tests for function calculating the local probability of action"""
+
     def test_p_action_2_action(self, n_actions=2) -> None:
         state_space = me.get_state_space(n_actions)
         n_states = me.get_n_states(n_actions)
         reward = np.zeros(n_states) + 1
         p_action = me.get_p_action(n_states, n_actions, reward, state_space)
 
-        assert p_action[0,1] == 1/2
-        assert p_action[0,0] == 1/2
-        assert p_action[1,0] == 1
-        assert p_action[2,1] == 1
-    
+        assert p_action[0, 1] == 1 / 2
+        assert p_action[0, 0] == 1 / 2
+        assert p_action[1, 0] == 1
+        assert p_action[2, 1] == 1
 
     def test_p_action_4_action(self, n_actions=4) -> None:
         state_space = me.get_state_space(n_actions)
@@ -313,58 +318,65 @@ class TestPAction:
         reward = np.zeros(n_states) + 1
         p_action = me.get_p_action(n_states, n_actions, reward, state_space)
 
-        assert p_action[0,1] == 1/4
-        assert p_action[0,3] == 1/4
-        assert p_action[3,0] == 1/2
-        assert p_action[5,0] == 1/2
-        assert p_action[14,3] == 1
+        assert p_action[0, 1] == 1 / 4
+        assert p_action[0, 3] == 1 / 4
+        assert p_action[3, 0] == 1 / 2
+        assert p_action[5, 0] == 1 / 2
+        assert p_action[14, 3] == 1
+
 
 class TestExpectedSvfFromPolicy:
     """Tests for function that counts expected empirical feature counts"""
-    def test_3_actions(self, n_actions=2) -> None:
-        n_states = me.get_n_states(n_actions)
-        state_space = me.get_state_space(n_actions)
-        trajectories = [
-            [(0, 1), (1, 1)], 
-            [(0, 0), (0, 1)],
-            [(0, 0), (1, 0)],
-            [(0, 0), (1, 0), (1, 1)]
-        ]
-        p_transition = me.get_p_transition(n_states=n_states, n_actions=n_actions, state_space=state_space, trajectories=trajectories)
-        reward = np.zeros(n_states) + 1
-        p_action = me.get_p_action(n_states=n_states, n_actions=n_actions, 
-                                    reward=reward, state_space=state_space)
 
-        p_initial = np.zeros(n_states)
-        p_initial[0] = 1
-        _, delta_test = me.expected_svf_from_policy(p_transition, p_action, p_initial=p_initial, eps = 1e-5)
-        
-        for i in range( len(delta_test) - 1 ):
-            assert delta_test[i] > delta_test[i+1]
+    def test_2_actions(self, n_actions=2) -> None:
+        state_space = me.get_state_space(n_actions=n_actions)
+        n_states = me.get_n_states(n_actions=n_actions)
+        mdp = me.DeterministicTreeMDP(n_actions=n_actions)
+        p_transition = me.get_p_transition(n_actions=n_actions, state_space=state_space, mdp=mdp)
+        featurizer = me.IdentityFeaturizer(n_actions=n_actions)
+        features = me.get_features(featurizer, state_space)
+        theta = np.zeros((len(features[0]),)) + 1
+        reward = features.dot(theta)
+        p_action = me.get_p_action(
+            n_states=n_states, n_actions=n_actions, reward=reward, state_space=state_space
+        )
+        e_svf = me.expected_svf_from_policy(
+            n_actions=n_actions, p_transition=p_transition, p_action=p_action
+        )
 
-    def test_3_actions(self, n_actions=3) -> None:
-        n_states = me.get_n_states(n_actions)
-        state_space = me.get_state_space(n_actions)
-        trajectories = [
-                [(0, 0, 0), (0, 1, 0)], 
-                [(0, 0, 0), (0, 0, 1)],
-                [(0, 0, 0), (1, 0, 0)],
-                [(0, 0, 0), (1, 0, 0), (1, 1, 0)]
-            ]
-        p_transition = me.get_p_transition(n_states=n_states, n_actions=n_actions, state_space=state_space, trajectories=trajectories)
-        reward = np.zeros(n_states) + 1
-        p_action = me.get_p_action(n_states=n_states, n_actions=n_actions, 
-                                    reward=reward, state_space=state_space)
+        assert e_svf[0] == 1
+        assert e_svf[1] == 0.5
+        assert e_svf[2] == 0.5
+        assert e_svf[3] == 1
 
-        p_initial = np.zeros(n_states)
-        p_initial[0] = 1
-        _, delta_test = me.expected_svf_from_policy(p_transition, p_action, p_initial=p_initial, eps = 1e-5)
-        
-        for i in range( len(delta_test) - 1 ):
-            assert delta_test[i] > delta_test[i+1]
+    def test_4_actions(self, n_actions=4) -> None:
+        state_space = me.get_state_space(n_actions=n_actions)
+        n_states = me.get_n_states(n_actions=n_actions)
+        mdp = me.DeterministicTreeMDP(n_actions=n_actions)
+        p_transition = me.get_p_transition(n_actions=n_actions, state_space=state_space, mdp=mdp)
+        featurizer = me.IdentityFeaturizer(n_actions=n_actions)
+        features = me.get_features(featurizer, state_space)
+        theta = np.zeros((len(features[0]),)) + 1
+        reward = features.dot(theta)
+        p_action = me.get_p_action(
+            n_states=n_states, n_actions=n_actions, reward=reward, state_space=state_space
+        )
+        e_svf = me.expected_svf_from_policy(
+            n_actions=n_actions, p_transition=p_transition, p_action=p_action
+        )
 
+        state1 = state_space.index(tuple([0, 0, 0, 0]))
+        state2 = state_space.index(tuple([1, 0, 0, 0]))
+        state3 = state_space.index(tuple([1, 1, 1, 0]))
+
+        assert e_svf[state1] == 1
+        assert e_svf[state2] == 0.25
+        assert e_svf[state3] == 0.25
+
+
+"""
 class TestIRL:
-    """ Tests for the function that performs the descent graident optimization algorithm """
+    Tests for the function that performs the descent graident optimization algorithm
     def test_2_actions(self, n_actions = 2):
 
         trajectories = [
@@ -381,24 +393,21 @@ class TestIRL:
         n_states = me.get_n_states(n_actions)
         counts = me.expected_empirical_feature_counts(mdp=mdp, featurizer=featurizer, trajectories=trajectories)
         fe = sum(np.array(list(counts.values())))
-        optim = O.ExpSga(lr=O.linear_decay(lr0=0.2), normalize=True)
-        p_initial = np.zeros(n_states)
-        p_initial[0] = 1
+        optim = O.Sga(lr=0.2)
+        
 
-        _, delta_test, _ = me.irl(n_actions,
-                    features=features, 
-                    feature_expectation=fe, 
-                    optim=optim, 
-                    p_initial=p_initial, 
-                    eps=1e-4, 
-                    eps_esvf=1e-5, trajectories=trajectories)
+        _, delta_test, _, _, _ = me.irl(n_actions=n_actions, features=features, feature_expectation=fe, optim = optim, 
+                 eps=1e-4, mdp=mdp)
 
         
 
         for i in range( len(delta_test) - 1 ):
             assert delta_test[i] > delta_test[i+1]
 
-    def test_2_actions(self, n_actions = 3):
+    
+
+    
+    def test_3_actions(self, n_actions = 3):
 
         trajectories = [
                 [(0, 0, 0), (0, 1, 0)], 
@@ -414,11 +423,13 @@ class TestIRL:
         n_states = me.get_n_states(n_actions)
         counts = me.expected_empirical_feature_counts(mdp=mdp, featurizer=featurizer, trajectories=trajectories)
         fe = sum(np.array(list(counts.values())))
-        optim = O.ExpSga(lr=O.linear_decay(lr0=0.2), normalize=True)
+        #optim = O.ExpSga(O.exponential_decay(lr0=0.01))
+        optim = O.ExpSga(lr=0.01)
+        
         p_initial = np.zeros(n_states)
         p_initial[0] = 1
 
-        _, delta_test, _ = me.irl(n_actions,
+        _, delta_test, _, _ = me.irl(n_actions,
                     features=features, 
                     feature_expectation=fe, 
                     optim=optim, 
@@ -428,7 +439,4 @@ class TestIRL:
 
         for i in range( len(delta_test) - 1 ):
             assert delta_test[i] > delta_test[i+1]
-
-
-
-
+"""
