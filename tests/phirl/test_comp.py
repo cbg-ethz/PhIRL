2caa4b4910a3ce436f1b3e780e0f6d66ec09e30d
Jiayi-Wang-Joey <61793140+Jiayi-Wang-Joey@users.noreply.github.com>
2022-04-12 10:34:03 +0200

    Update test_comp.py

--- tests/phirl/test_comp.py
+++ tests/phirl/test_comp.py
@@ -1,15 +1,14 @@
 def test_protocol() -> None:
+    from phirl._comp import Protocol
 
-     from phirl._comp import Protocol
+    class Interface(Protocol):
+        def a(self) -> int:
+            pass
 
-     class Interface(Protocol):
-         def a(self) -> int:
-             pass
+    class Implementation(Interface):
+        def a(self) -> int:
+            return 3
 
-     class Implementation(Interface):
-         def a(self) -> int:
-             return 3
-
-     x = Implementation()
-     assert x.a() == 3
+    x = Implementation()
+    assert x.a() == 3
 
