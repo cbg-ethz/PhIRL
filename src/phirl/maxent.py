41a37097f25e2646bc161e3d90703ffdee2e7e9f
Paweł Czyż <pawelc2010@gmail.com>
2022-05-12 17:52:21 +0200

    Trajectory format converter

--- src/phirl/maxent.py
+++ src/phirl/maxent.py
@@ -1,11 +1,13 @@
 """Maximum Entropy IRL, powered by the irl_maxent module:
 https://github.com/qzed/irl-maxent/
 """
-from typing import List, Sequence, TypeVar
+from typing import Iterable, List, Sequence, TypeVar
 
+import irl_maxent.trajectory
 import numpy as np
 
 import phirl.mdp.interfaces as mdp_int
+import phirl.mdp.common as common
 import phirl.enumerate as en
 
 _S = TypeVar("_S")
@@ -30,12 +32,21 @@ def get_features(
 def get_p_transition(
     params: mdp_int.IMDPParams[_S, _A], dynamics: mdp_int.IDeterministicTransitionFunction[_S, _A]
 ) -> np.ndarray:
-    """
+    """Returns the transition probability matrix.
+
+    Args:
+        params: MDP parameters
+        dynamics: deterministic transition function
 
     Returns:
         transition matrix, shape (n_states, n_states, n_actions).
          At index (state_from, state_to, action)
          there is the probability P(state_to | state_from, action)
+
+    Note:
+        This matrix contains a lot of redundant information -- we have
+         `n_actions * n_states` ones and the rest of the entries
+         are zeros. (I.e., only `1/n_states` fraction of entries is non-zero).
     """
     p = np.zeros((params.n_states, params.n_states, params.n_actions))
 
@@ -53,5 +64,35 @@ def get_p_transition(
 
 def get_terminal(params: mdp_int.IMDPParams, terminal_states: Sequence[_S]) -> List[int]:
     mapping = en.Enumerate(params.states)
-
     return [mapping.to_index(state) for state in terminal_states]
+
+
+def convert_trajectory(
+    params: mdp_int.IMDPParams[_S, _A], trajectory: common.Trajectory[_S, _A]
+) -> irl_maxent.trajectory.Trajectory:
+    mapping_states = en.Enumerate(params.states)
+    mapping_actions = en.Enumerate(params.actions)
+
+    transitions = []
+
+    for i, action in enumerate(trajectory.actions):
+        state_from: _S = trajectory.states[i]
+        state_to: _S = trajectory.states[i + 1]
+
+        # Remember to pass from state and action
+        # objects to 0-indexed indices.
+        # We want tuples (state_from, action, state_to)
+        transitions.append(
+            (
+                mapping_states.to_index(state_from),
+                mapping_actions.to_index(action),
+                mapping_states.to_index(state_to),
+            )
+        )
+    return irl_maxent.trajectory.Trajectory(transitions)
+
+
+def get_trajectories(
+    params: mdp_int.IMDPParams[_S, _A], trajectories: Iterable[common.Trajectory[_S, _A]]
+) -> List[irl_maxent.trajectory.Trajectory]:
+    return [convert_trajectory(params=params, trajectory=t) for t in trajectories]
